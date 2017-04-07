a190297fc2ca5fdcf410938a0bb2f7afa3357dda
molecule <molecule@berkeley.edu>
2017-04-06 18:15:20 -0700

    Update h-bridge to support another input and output, and generalize the code a bit.

--- h-bridge/h-bridge.ino
+++ h-bridge/h-bridge.ino
@@ -1,7 +1,16 @@
 int hBridgeInput1 = 4;
-int hBridgeInput2 = 2; // white
+int hBridgeInput2 = 2; 
+int hBridgeInput4 = 6;
+int hBridgeInput3 = 7;
 int speedPinA = 3; // Needs to be a PWM pin to be able to control motor speed.
+int speedPinB = 5;
 int led = 13;
+
+#define NUM_INPUTS 4
+int hBridgeInput[NUM_INPUTS] = {
+  4, 2, 6, 7
+};
+
 enum dir {
   FORWARD,
   BACKWARD
@@ -12,23 +21,34 @@ void setDirection(dir newDir);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(hBridgeInput1, OUTPUT);
-  pinMode(hBridgeInput2, OUTPUT);
+  for (int i = 0; i < NUM_INPUTS; i++) {
+    pinMode(hBridgeInput[i], OUTPUT);
+  }
   pinMode(led, OUTPUT);
 }
+
 void setDirection(dir newDir) {
   if (newDir == FORWARD) {
-    //Serial.println("Forward");
-    digitalWrite(hBridgeInput1, LOW);
-    digitalWrite(hBridgeInput2, HIGH);
     digitalWrite(led, HIGH);
+    for (int i = 0; i < NUM_INPUTS; i++) {
+      if (i%2 == 0) {
+        digitalWrite(hBridgeInput[i], LOW);
+      } else {
+        digitalWrite(hBridgeInput[i], HIGH);
+      } 
+    }
   } else {
-    //Serial.println("Backward");
-    digitalWrite(hBridgeInput1, HIGH);
-    digitalWrite(hBridgeInput2, LOW);
-    digitalWrite(led, LOW);
-  }
+      digitalWrite(led, LOW);
+      for (int i = 0; i < NUM_INPUTS; i++) {
+        if (i%2 == 0) {
+          digitalWrite(hBridgeInput[i], HIGH);
+        } else {
+          digitalWrite(hBridgeInput[i], LOW);
+        }
+      }
+    }
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
    if (currDir == FORWARD) {
@@ -36,9 +56,10 @@ void loop() {
   } else {
     currDir = FORWARD;
   }
-  Serial.print("currDir:");
+  Serial.print("currDir: ");
   Serial.println(currDir);
   setDirection(currDir);
-  analogWrite(speedPinA, 100);
-  delay(5000);
+  analogWrite(speedPinA, 150);
+  analogWrite(speedPinB, 150);
+  delay(3000);
 }
